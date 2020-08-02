71f2f0ab8cd6ac778e16e632ba7e3cf9a03ef88d
Chinmoy Chakraborty <chinmoy12c@gmail.com>
2020-08-02 21:01:07 +0530

    exp9 simulator

--- js/utils.js
+++ js/utils.js
@@ -8,6 +8,15 @@ runExperiment1 = () => {
         answer.innerHTML = inp3;
     }
     else {
-        answer.innerHTML = Math.abs(inp1 - inp2);
+        answer.innerHTML = inp3 * Math.sin((inp1 - inp2) * Math.PI/180);
     }
+}
+
+runExperiment2 = () => {
+    let inp1 = parseFloat(document.getElementById("inp1").value);
+    let inp2 = parseFloat(document.getElementById("inp2").value);
+    let inp3 = parseFloat(document.getElementById("inp3").value);
+    let answer = document.getElementById("answer");
+
+    answer.innerHTML = inp3 * Math.sin((inp1 - inp2) * Math.PI/180);
 }
